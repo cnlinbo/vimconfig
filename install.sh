81e9a948f9d00dab88dc4cda7b3804e2824c05e2
cnlinbo <cnlinbo@outlook.com>
2021-12-01 15:32:11 +0800

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 project_path=$(cd `dirname $0`;pwd)
+echo "Copying .vim and .vimrc to your home directory..."
+echo "$project_path/.vimrc -> $HOME/.vimrc"
 /bin/cp $project_path/.vimrc ~
+echo "$project_path/.vim -> $HOME/.vim"
 /bin/cp -r $project_path/.vim ~
 if [ "$1" == "desktop" ]; then
+    echo "Downloading markdown-preview binary file..."
     ~/.vim/plugged/markdown-preview.nvim/app/install.sh
 fi
+echo "Install successfully!"
+
