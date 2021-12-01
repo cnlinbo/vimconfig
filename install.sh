17cb812841b46fad0280b1b69a1fb4b722f5ea11
cnlinbo <cnlinbo@outlook.com>
2021-12-01 15:15:44 +0800

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 project_path=$(cd `dirname $0`;pwd)
-/bin/cp $project_path/.vimrc ~/.vimrc
-/bin/cp -r $project_path/.vim ~/.vim
+/bin/cp $project_path/.vimrc ~
+/bin/cp -r $project_path/.vim ~
 if [ "$1" == "desktop" ]; then
     ~/.vim/plugged/markdown-preview.nvim/app/install.sh
 fi
